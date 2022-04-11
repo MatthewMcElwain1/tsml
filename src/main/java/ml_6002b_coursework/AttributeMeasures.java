78bb57ec2436acc1dbfb711e12f6e6a11b64ec23
MatthewMcElwain1 <58671229+MatthewMcElwain1@users.noreply.github.com>
2022-04-11 10:30:15 +0100

    attribute measures root entropy calcs

--- src/main/java/ml_6002b_coursework/AttributeMeasures.java
+++ src/main/java/ml_6002b_coursework/AttributeMeasures.java
@@ -49,19 +49,38 @@ public class AttributeMeasures {
             }
         }
 
-        //set up variables for final calculation, set entropy of root node to 1 as a baseline
-        double ig = 1;
-        double node_value;
+        //calculating the entropy of the root node
+        ArrayList<Integer> root_cases = new ArrayList<Integer>();
+        for (int i = 0; i < data[0].length; i++){
+            root_cases.add(i,0);
+        }
+
+        int temp1;
+        for (int[] Attribute : data) {
+            for (int i = 0; i < Attribute.length; i++){
+                temp1 = root_cases.get(i);
+                temp1 += Attribute[i];
+                root_cases.remove(i);
+                root_cases.add(i,temp1);
+            }
+        }
+
+        double root_entropy = 0;
+        for (Integer root_case : root_cases) {
+            root_entropy += (double) root_case/total_cases*log2((double) root_case/total_cases);
+        }
+        root_entropy = -root_entropy;
 
         //for each attribute value, calculate the weighted entropy and take it away from the root entropy
+        double node_value;
+
         for (int i = 0; i < attribute_value_entropy.size(); i++){
             node_value = ((double)attribute_value_cases.get(i)/total_cases)*attribute_value_entropy.get(i);
-            ig -= node_value;
+            root_entropy -= node_value;
         }
 
         //return the information gain rounded to two decimal places
-        return (double) Math.round(ig*100)/100;
-
+        return (double) Math.round(root_entropy*100)/100;
     }
 
     public static double measureInformationGainRatio(int[][] data){
@@ -85,13 +104,35 @@ public class AttributeMeasures {
         }
 
         return (double) Math.round(ig/-split_info*100)/100;
-
-
     }
 
     public static double measureGini(int[][] data){
-        double test = 1;
-        return test;
+        int total_cases = 0;
+
+        for (int[] attribute : data) {
+            for (int value : attribute) {
+                total_cases += value;
+            }
+        }
+
+        ArrayList<Double> probabilities = new ArrayList<Double>();
+
+        for (int[] attribute : data) {
+            int local_cases = 0;
+
+            for (int value : attribute) {
+               local_cases += value;
+            }
+
+            probabilities.add(Math.pow((double) local_cases/total_cases, 2));
+        }
+
+        double gini = 1;
+        for (Double probability : probabilities) {
+            gini -= probability;
+        }
+
+        return gini;
 
     }
 
@@ -106,9 +147,9 @@ public class AttributeMeasures {
      * @param args the options for the attribute measure main
      */
     public static void main(String[] args) {
-        int[][] data = {{4,0},{1,5}};
+        int[][] data = {{1,3},{0,2}};
 
-        System.out.println(measureInformationGainRatio(data));
+        System.out.println(measureInformationGain(data));
 
     }
 
