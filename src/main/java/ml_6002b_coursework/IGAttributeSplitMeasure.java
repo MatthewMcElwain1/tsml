4c3f64de7da3c4a7669166321ea677cc4f878022
MatthewMcElwain1 <58671229+MatthewMcElwain1@users.noreply.github.com>
2022-05-02 17:57:48 +0100

    part 2.2.1 added comments

--- src/main/java/ml_6002b_coursework/IGAttributeSplitMeasure.java
+++ src/main/java/ml_6002b_coursework/IGAttributeSplitMeasure.java
@@ -15,29 +15,34 @@ public class IGAttributeSplitMeasure extends AttributeSplitMeasure {
 
     @Override
     public double computeAttributeQuality(Instances data, Attribute att, Boolean useGain) throws Exception {
-        // first split the data by attribute
+        // first split the data by attribute value
         Instances[] split_data = splitData(data, att);
+        // setup attr split array
         int[][] attr_split_array = new int[split_data.length][];
-
+        // loop through the split data
         for (int x = 0; x < split_data.length; x++){
+                // set the class attribute
                 split_data[x].setClassIndex(split_data[x].numAttributes()-1);
+                // setup class distribution array
                 int[] class_dist = new int[split_data[x].numClasses()];
-
+                // loop through the instances x (how many classes) number of times
                 for (double i =0.0; i < split_data[x].numClasses(); i++){
                     for (Instance instance:split_data[x]){
+                        // if the class value of the instance is the same as the current class loop add one to the correct number in the array
                         if (instance.classValue() == i){
                             class_dist[(int) i] += 1;
                         }
                     }
                 }
+            // adding the class dist array to the main split array
             attr_split_array[x] = class_dist;
             }
-
+        // determining whether to use gain ratio or not and then returning final number
         if (useGain){
-            return measureInformationGainRatio(attr_split_array);
+            return measureInformationGain(attr_split_array);
         }
         else{
-            return measureInformationGain(attr_split_array);
+            return measureInformationGainRatio(attr_split_array);
         }
     }
 
