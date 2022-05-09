ef79a11b1f055373f7c62acced9a994f6f411825
MatthewMcElwain1 <58671229+MatthewMcElwain1@users.noreply.github.com>
2022-05-09 23:54:00 +0100

    ensemble part 2 done

--- src/main/java/ml_6002b_coursework/TreeEnsemble.java
+++ src/main/java/ml_6002b_coursework/TreeEnsemble.java
@@ -17,11 +17,11 @@ import java.util.Random;
 
 public class TreeEnsemble extends AbstractClassifier {
 
-    private int numTrees;
+    private final int numTrees;
 
-    private double subset_percentage;
+    private final double subset_percentage;
 
-    CourseworkTree[] classifiers;
+    ArrayList<CourseworkTree> classifiers = new ArrayList<CourseworkTree>();
 
     int[][] att_selections;
 
@@ -40,11 +40,12 @@ public class TreeEnsemble extends AbstractClassifier {
         // creating random subsets and removing random attributes
         Random rand = new Random();
 
-        att_selections = new int[numTrees][(int) (data.numAttributes()*0.5)+1];
+        att_selections = new int[numTrees][(int) (data.numAttributes()*subset_percentage)+1];
 
         int att_index;
 
-        int[] atts_to_keep = new int[(int) (data.numAttributes()*0.5)+1];
+        int[] atts_to_keep = new int[(int) (data.numAttributes()*subset_percentage)+1];
+
 
         Remove removeFilter = new Remove();
 
@@ -57,21 +58,41 @@ public class TreeEnsemble extends AbstractClassifier {
                 att_list.add(y);
             }
 
-            for (int x = 0; x < (int)data.numAttributes()*0.5; x++){
+            for (int x = 0; x < (int)data.numAttributes()*subset_percentage; x++){
                 att_index = rand.nextInt(att_list.size()-1);
                 atts_to_keep[x] = att_list.get(att_index);
                 att_list.remove(att_index);
             }
 
+            atts_to_keep[atts_to_keep.length-1] = data.numAttributes()-1;
             removeFilter.setAttributeIndicesArray(atts_to_keep);
             removeFilter.setInvertSelection(true);
             removeFilter.setInputFormat(data);
             folds[i] = Filter.useFilter(data, removeFilter);
             att_list.clear();
             att_selections[i] = atts_to_keep;
-            atts_to_keep = new int[(int) (data.numAttributes()*0.5)+1];
+            atts_to_keep = new int[(int) (data.numAttributes()*subset_percentage)+1];
         }
-        
+
+        for (Instances fold:folds){
+            CourseworkTree tree = new CourseworkTree();
+            switch(rand.nextInt(3)+1){
+                case 1:
+                    tree.setAttSplitMeasure(new IGAttributeSplitMeasure(true));
+                    break;
+                case 2:
+                    tree.setAttSplitMeasure(new IGAttributeSplitMeasure(false));
+                    break;
+                case 3:
+                    tree.setAttSplitMeasure(new GiniAttributeSplitMeasure());
+                    break;
+                default:
+                    tree.setAttSplitMeasure(new ChiSquaredAttributeSplitMeasure());
+                    break;
+            }
+            tree.buildClassifier(fold);
+            classifiers.add(tree);
+        }
 
 
 
