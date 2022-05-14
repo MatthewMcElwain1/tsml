c2f005297367c5d81ab04e1c6ea0f5131cd9a24c
MatthewMcElwain1 <58671229+MatthewMcElwain1@users.noreply.github.com>
2022-05-14 19:58:34 +0100

    tiny bits

--- src/main/java/ml_6002b_coursework/Experiments.java
+++ src/main/java/ml_6002b_coursework/Experiments.java
@@ -327,82 +327,19 @@ public class Experiments {
 
 
         public static void experiment_3() throws Exception {
-
-            // -------------getting datasets into Instances format----------------------------------
-
-            String[] discrete_dataset_names = DatasetLists.nominalAttributeProblems;
-            String[] continuous_dataset_names = DatasetLists.continuousAttributeProblems;
-
-            Instances[] discrete_datasets = new Instances[DatasetLists.nominalAttributeProblems.length];
-            Instances[] continuous_datasets = new Instances[DatasetLists.continuousAttributeProblems.length];
-
-            String discrete_file_location = "src/main/java/ml_6002b_coursework/UCI Discrete";
-            String continous_file_location = "src/main/java/ml_6002b_coursework/UCI Continuous";
-
-            String file_location;
-            BufferedReader reader;
-            Instances data;
-
-            for (int i = 0; i < discrete_dataset_names.length; i++){
-                file_location = String.format("%s/%s/%s.arff", discrete_file_location, discrete_dataset_names[i], discrete_dataset_names[i]);
-                reader = new BufferedReader(new FileReader(file_location));
-                data = new Instances(reader);
-                data.setClassIndex(data.numAttributes()-1);
-                discrete_datasets[i] = data;
-            }
-
-            for (int i = 0; i < continuous_dataset_names.length; i++){
-                file_location = String.format("%s/%s/%s.arff", continous_file_location, continuous_dataset_names[i], continuous_dataset_names[i]);
-                reader = new BufferedReader(new FileReader(file_location));
-                data = new Instances(reader);
-                data.setClassIndex(data.numAttributes()-1);
-                continuous_datasets[i] = data;
-            }
-
-            // --------------making train test splits-------------------------------
-
-            StratifiedRemoveFolds filter = new StratifiedRemoveFolds();
-            String[] options = new String[6];
-            Random rand = new Random();
-            int seed = rand.nextInt(5000)+1;
-            options[0] = "-N";
-            options[1] = Integer.toString(5);
-            options[2] = "-S";
-            options[3] = Integer.toString(seed);
-            options[4] = "-F";
-            options[5] = Integer.toString(1);
-
-            filter.setOptions(options);
-
-            Instances[] discrete_train_splits = new Instances[discrete_datasets.length];
-            Instances[] discrete_test_splits = new Instances[discrete_datasets.length];
-
-            Instances[] continuous_train_splits = new Instances[continuous_datasets.length];
-            Instances[] continuous_test_splits = new Instances[continuous_datasets.length];
-
-
-            for (int i = 0; i < discrete_datasets.length; i++){
-                filter.setInputFormat(discrete_datasets[i]);
-                filter.setInvertSelection(false);
-                discrete_test_splits[i] = Filter.useFilter(discrete_datasets[i], filter);
-                filter.setInvertSelection(true);
-                discrete_train_splits[i] = Filter.useFilter(discrete_datasets[i], filter);
-            }
-
-            for (int i = 0; i < continuous_datasets.length; i++){
-                filter.setInputFormat(continuous_datasets[i]);
-                filter.setInvertSelection(false);
-                continuous_test_splits[i] = Filter.useFilter(continuous_datasets[i], filter);
-                filter.setInvertSelection(true);
-                continuous_train_splits[i] = Filter.useFilter(continuous_datasets[i], filter);
-            }
-
             // ---------------- setting up variables for experiment-------------
 
             int correct;
             int total;
             double prediction;
             double accuracy;
+
+            Instances[][] test_train_splits = prepare_testTrain_split();
+
+            Instances[] discrete_train_splits = test_train_splits[0];
+            Instances[] discrete_test_splits = test_train_splits[1];
+            Instances[] continuous_train_splits = test_train_splits[2];
+            Instances[] continuous_test_splits = test_train_splits[3];
 
 
             // --------------- coursework ensemble---------------
@@ -517,7 +454,7 @@ public class Experiments {
 
 
     public static void main(String[] args) throws Exception {
-        Experiments.experiment_1();
-        //Experiments.experiment_3();
+        //Experiments.experiment_1();
+        Experiments.experiment_3();
     }
 }
